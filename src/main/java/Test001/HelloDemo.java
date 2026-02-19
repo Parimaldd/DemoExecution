1acfee8916029f0aaebdec19818f7847f982b0ec
Parimal Todsam <mrunalganvir1111@gmail.com>
2026-02-19 21:15:26 +0530

    Committing changes for master code

--- src/main/java/Test001/HelloDemo.java
+++ src/main/java/Test001/HelloDemo.java
@@ -2,6 +2,6 @@ package Test001;
 
 public class HelloDemo {
     public static void main(String[] args) {
-        System.out.println("Mrunal from master");
+        System.out.println("master-this is my first commit ");
     }
 }
